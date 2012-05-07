c40dd4ae0c59c2853e69721f2c00e4368cf2b916
ezequiel.castellano <ezequiel.castellano@4c09266d-6570-e7e6-a575-67d11a4290b3>
2012-05-07 04:02:24 +0000

    Se actualizó el archivo bueno. Se emprolijó el analizador.

--- Tokenizador/src/Analizador.java
+++ Tokenizador/src/Analizador.java
@@ -8,39 +8,15 @@ public class Analizador {
 	public static List<Token> obtenerTokens(String palabra) {
 		List<Token> tokens = new ArrayList<Token>();
 		List<Token> tokensTraseros = new ArrayList<Token>();
-		String resto = palabra;
+		String resto = palabra.trim();
 		while (!resto.isEmpty()) {
-			if(resto.matches(Regex.palabra)){
-				System.out.println("Palabra...");
-				tokens.add(new Token(resto,Regla.palabra));
-				resto = "";
-			}
-			else if(resto.matches(Regex.espacio)){
-				System.out.println("Esacio...");
-				resto = "";
-			}
-			else if(resto.matches(Regex.numero)){
-				System.out.println("Numero...");
-				tokens.add(new Token(resto,Regla.numero));
-				resto = "";
-			}
-			else if(resto.matches(Regex.guion)){
-				System.out.println("Palabra con guion intermedio...");
-				tokens.add(new Token(resto,Regla.palabra_compuesta));
-				resto = "";
-			}
-			else if(resto.matches(Regex.barra)){
-				System.out.println("Palabra con barra...");
-				tokens.add(new Token(resto,Regla.palabra_barra));
-				resto = "";
-			}
-			else if(resto.matches(Regex.porcentaje)){
-				System.out.println("Porcentaje...");
-				tokens.add(new Token(resto,Regla.porcentaje));
+			if(resto.matches(Regex.palabra +"|"+ Regex.numero +"|"+ Regex.guion +"|"+ Regex.barra +"|"+ Regex.porcentaje
+					+"|"+ Regex.simbolo +"|"+ Regex.abreviatura +"|"+ Regex.nombres)){
+				tokens.add(obtenerTokenEspecifico(resto));
 				resto = "";
 			}
 			else if(diccionarioContracciones.esUnaContraccion(resto)){
-				System.out.println("Es una contraccion...");
+				//System.out.println("Es una contraccion...");
 				String formaNormal = diccionarioContracciones.obtenerFormaNormal(resto);
 				for (String parte : formaNormal.split(" ")) {
 					tokens.addAll(Analizador.obtenerTokens(parte));
@@ -48,51 +24,61 @@ public class Analizador {
 				resto = "";
 			}
 			else if(resto.matches(Regex.apostrofe)){
-				System.out.println("Es un posesivo o algo raro...");
+				//System.out.println("Es un posesivo o algo raro...");
 				String[] partes = resto.split("'");
 				assert(partes.length == 2);
 				tokens.add(new Token(partes[0], Regla.palabra));
 				tokens.add(new Token("'"+partes[1], Regla.apostrofe));
 				resto = "";
 			}
-			else if(resto.matches(Regex.abreviatura)){
-				System.out.println("Es una abreviatura");
-				tokens.add(new Token(resto,Regla.abreviatura));
-				resto = "";
-			}
-			else if(resto.matches(Regex.simbolo)){
-				System.out.println("Es un simbolo");
-				tokens.add(new Token(resto,Regla.simbolo));
-				resto = "";
-			}
-			else if(resto.matches(Regex.palabra_numero+Regex.simbolos)){
-				System.out.println("Es una palabra que termina en un simbolo");
+			else if(resto.matches(".+"+Regex.simbolo)){
+				//System.out.println("Es una palabra que termina en un simbolo");
 				tokensTraseros.add(0, new Token(resto.substring(resto.length()-1), Regla.simbolo));
 				resto = resto.substring(0,resto.length()-1);
 			}else if(resto.matches(Regex.simbolo+".+")){
-				System.out.println("Empieza en un simbolo");
+				//System.out.println("Empieza en un simbolo");
 				tokens.add(new Token(resto.substring(0,1), Regla.simbolo));
 				resto = resto.substring(1);
 			}
-			else if(resto.matches(Regex.nombres)){
-				System.out.println("Es un nombre...");
-				tokens.add(new Token(resto, Regla.nombres));
-				resto = "";
-			}
-			else if(resto.matches(Regex.nombres+Regex.simbolos)){
-				System.out.println("Es un nombre termina en un simbolo");
-				tokensTraseros.add(0, new Token(resto.substring(resto.length()-1), Regla.simbolo));
-				resto = resto.substring(0,resto.length()-1);
-			}	
 			else {
-				System.out.println("Algo más complejo que una palabra...");
+				//System.out.println("Algo más complejo que una palabra...");
 				tokens.add(new Token(resto, Regla.raro));
 				resto = "";
-				
-			} 
+			} 	
 		}
 		tokens.addAll(tokensTraseros);
 		return tokens;
+	}
+	
+	private static Token obtenerTokenEspecifico(String palabra){
+		Token token = null;
+		
+		if(palabra.matches(Regex.palabra)){
+			token = (new Token(palabra,Regla.palabra));
+		}
+		else if(palabra.matches(Regex.numero)){
+			token = (new Token(palabra,Regla.numero));
+		}
+		else if(palabra.matches(Regex.guion)){
+			token = (new Token(palabra,Regla.palabra_compuesta));
+		}
+		else if(palabra.matches(Regex.barra)){
+			token = (new Token(palabra,Regla.palabra_barra));
+		}
+		else if(palabra.matches(Regex.porcentaje)){
+			token = (new Token(palabra,Regla.porcentaje));
+		}
+		else if(palabra.matches(Regex.abreviatura)){
+			token = (new Token(palabra,Regla.abreviatura));
+		}
+		else if(palabra.matches(Regex.simbolo)){
+			token = (new Token(palabra,Regla.simbolo));
+		}
+		else if(palabra.matches(Regex.nombres)){
+			token = (new Token(palabra, Regla.nombres));
+		}
+		
+		return token;
 	}
 	
 	
